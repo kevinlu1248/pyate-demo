9e99e131a873c7bc107dcf0df1d16ab1f4ce6a0d
Kevin <kevinlu1248@gmail.com>
2021-06-25 22:08:14 -0700

    changed error handling

--- server/app.py
+++ server/app.py
@@ -34,25 +34,25 @@ def ate():
 
 
 # boilerplate code from https://github.com/realpython/flask-boilerplate/blob/master/app.py
-@app.errorhandler(500)
-def internal_error(error):
-    return render_template("errors/500.html"), 500
+# @app.errorhandler(500)
+# def internal_error(error):
+    # return render_template("errors/500.html"), 500
 
 
-@app.errorhandler(404)
-def not_found_error(error):
-    return render_template("errors/404.html"), 404
+# @app.errorhandler(404)
+# def not_found_error(error):
+    # return render_template("errors/404.html"), 404
 
 
-if not app.debug:
-    file_handler = FileHandler("error.log")
-    file_handler.setFormatter(
-        Formatter("%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]")
-    )
-    app.logger.setLevel(logging.INFO)
-    file_handler.setLevel(logging.INFO)
-    app.logger.addHandler(file_handler)
-    app.logger.info("errors")
+# if not app.debug:
+    # file_handler = FileHandler("error.log")
+    # file_handler.setFormatter(
+        # Formatter("%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]")
+    # )
+    # app.logger.setLevel(logging.INFO)
+    # file_handler.setLevel(logging.INFO)
+    # app.logger.addHandler(file_handler)
+    # app.logger.info("errors")
 
 if __name__ == "__main__":
     app.run()
